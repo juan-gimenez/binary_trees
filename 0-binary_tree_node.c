a1eecf83d56d823572e5eb937a8d8c86c7e00cb3
juan-gimenez <juancitogimenez0@gmail.com>
2022-03-22 07:13:05 -0700

    task 0 and 1 with betty checked

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,35 +1,21 @@
 #include "binary_trees.h"
 
- /**
-  * binary_tree_insert_left - inserts a node
-  *@parent : pointer to the node to insert the left-child in
-  *@value: value
-  * Return: 0
-  */
+/**
+ * binary_tree_node - creates a binary tree node
+ * @parent:pointer to the parent node
+ * @value:value to insert
+ *
+ * Return: pointer to the created node
+ */
 
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-  binary_tree_t *tmp;
-
-  if (!parent)
-    return (NULL);
-  tmp = malloc(sizeof(binary_tree_t));
-  /**
-     chech if malloc fails
-  */
-  if (!temp)
-    return (NULL);
-  tmp->left = tmp->right = NULL;
-  tmp->n = value;
-  tmp->parent = parent;
-  if (parent->left)
-    {
-      parent->left->parent = tmp;
-      tmp->left = parent->left;
-    }
-  parent->left = tmp;
-  /**
-return the new node with value
-  */
-  return (tmp);
+binary_tree_t *node = malloc(sizeof(binary_tree_t));
+if (node)
+return (NULL);
+node->left = NULL;
+node->right = NULL;
+node->n = value;
+node->parent = parent;
+return (node);
 }

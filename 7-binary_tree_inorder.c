75e546b9f8fa626454b1832228218cb071fea5e4
juan-gimenez <juancitogimenez0@gmail.com>
2022-03-23 13:35:10 -0700

    fixed compiler error

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -12,8 +12,8 @@ if (func == NULL || tree == NULL)
 return;
 else
 {
-binary_tree_preorder(tree->left, (*func));
+binary_tree_inorder(tree->left, (*func));
 func(tree->n);
-binary_tree_preorder(tree->right, (*func));
+binary_tree_inorder(tree->right, (*func));
 }
 }

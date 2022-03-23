10a5e297e429490131aa0a3a256c53e581463957
juan-gimenez <juancitogimenez0@gmail.com>
2022-03-23 14:13:03 -0700

    task 8 not checked yet

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ *binary_tree_postorder - goes through a binary tree using post-order traversal
+ *@tree: ptr to the root node
+ *@func: ptr to function
+ */
+
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+if (func == NULL || tree == NULL)
+return;
+else
+{
+binary_tree_preorder(tree->left, (*func));
+binary_tree_preorder(tree->right, (*func));
+func(tree->n);
+}
+}

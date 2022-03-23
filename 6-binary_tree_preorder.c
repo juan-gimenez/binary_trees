f1f3f4a4b0c9637832b7a74e0b6a50795f05274a
juan-gimenez <juancitogimenez0@gmail.com>
2022-03-23 12:50:50 -0700

    added else cause was not working in main

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+ /**
+  *binary_tree_preorder - goes through a binary tree using pre-order traversal
+  *@tree: ptr to the root node
+  *@func: ptr to function
+  */
+
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+if (func == NULL || tree == NULL)
+return;
+else
+{
+func(tree->n);
+binary_tree_preorder(tree->left, (*func));
+binary_tree_preorder(tree->right, (*func));
+}
+}

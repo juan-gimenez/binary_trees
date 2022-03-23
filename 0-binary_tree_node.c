10cd0ab3d483cd66c453b358ce1ae9f8ffa6e756
juan-gimenez <juancitogimenez0@gmail.com>
2022-03-23 10:42:45 -0700

    checked

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,7 +11,7 @@
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 binary_tree_t *node = malloc(sizeof(binary_tree_t));
-if (node)
+if (node == NULL)
 return (NULL);
 node->left = NULL;
 node->right = NULL;
